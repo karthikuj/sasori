6c60505ab3f809599d9d9506bf0bd609332c8f25
karthikuj <karthikuj2001@gmail.com>
2023-11-13 11:10:44 +0530

    Adds a todo: comment
    Signed-off-by: karthikuj <karthikuj2001@gmail.com>

--- src/crawler/crawlState.js
+++ src/crawler/crawlState.js
@@ -10,6 +10,7 @@ class CrawlState {
     //     1. Add equals method to compare CrawlStates.
     //     2. Add getStrippedDom method.
     //     3. Add depth property.
+    //     4. Add id to CrawlState.
 }
 
 module.exports = CrawlState;
